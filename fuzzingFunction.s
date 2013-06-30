cdc7175f4b019029c932f2c9d239c586e738d69e
wont <wont@wont-virtual-machine.(none)>
2013-06-30 17:26:10 +0000

    bugs

--- fuzzingFunction.s
+++ fuzzingFunction.s
@@ -8,7 +8,7 @@ fuzz:
 	push ebp
 	mov ebp,esp
 	xor eax,eax
-	mov ecx, [ebp-0xC]	;arg2
+	mov ecx, [ebp+0x8]	;arg1
 	cmp ecx,3		;arg testing there are only 3 fuzzing methods
 	jbe fuzz.writeStack		;if it's good continue with fuzzing
 	mov eax,-1
@@ -20,39 +20,40 @@ fuzz:
 	inc eax			;Here we just see what happens!
 	cmp eax, 16
 	jnl fuzz.fuzzer
-	jmp fuzz.writeStack 
+	jmp fuzz.writeStack
 
 .fuzzer:
 
-	mov eax, [ebp-0x8]	;arg1
 
 .fuzz:
-	mov eax, [ebp-0x8]	;arg1
+	mov eax, [ebp+0xc]	;arg2
 	cmp eax,0x100		;Test syscall number
-	jae fuzz.end		;exit if too high 
+	jae fuzz.end		;exit if too high
 	jmp [fuzzTable+4*ecx-4]	;fuzz based on the fuzzing method
 .finishCall:
 	inc dword [ebp-0x8]			;Increment syscall#
 	jmp fuzz.fuzz		;Try again
 
-	
+
 .end:				;fuzzing done, no crashes :(
 	add esp, 16*4
+	xor eax,eax
+
 	leave
 	ret
 
 
 	;; set up fuzzing stuff
 	;; I can't think of a better way to do this
-	
+
 sysent:
 	sysenter
 	jmp fuzz.finishCall
 callgate:
-	call [0xffff000c] 	;pointer to kernelgate
+	call [0xffff000c]
 	jmp fuzz.finishCall
 int99:
 	int 99
 	jmp fuzz.finishCall
 
-fuzzTable:	dd sysent,callgate,int99 ;important these match the c fuzzer
+fuzzTable:	dd int99,sysent,callgate ;important these match the c fuzzer
